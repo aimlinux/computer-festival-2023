7582317985f6a5031ebeae25c85c2bf7bc989799
aimlinux <1k8aioon@gmail.com>
2023-02-12 17:05:23 +0900

    commit_02.22

---
+++ python_code/test_02.12.py
@@ -0,0 +1,26 @@
+
+hiragana = ['あ','い','う','え','お','か','き','く','け','こ','さ',]
+
+var = list(input('文字：'))
+
+#var_num = range(100)
+for i in range(100):
+    print(var[i])
+    i = i + 1
+    
+
+
+
+
+
+#var_zip = [(i, j) for i, j in enumerate(var)]
+#print(var_zip)
+
+
+
+#listの一文字ずつを配列に入れる
+#for var in range(var_num):
+#var = ['num1', 'num2']
+#null_date = []
+#print(num)
+
