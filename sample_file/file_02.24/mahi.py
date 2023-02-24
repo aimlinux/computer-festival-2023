19eecb06a3cf8f18446af765338b733c55081e4a
Kohara <1k8aioon@gmail.com>
2023-02-24 17:34:31 +0900

    commit

--- sample_file/file_02.24/mahi.py
+++ sample_file/file_02.24/mahi.py
@@ -1,10 +1,10 @@
 import random as rnd
-f=open("text.bin","rb")
-sum=[0]*93
+f=open("hira.bin","rb")
+sum=[0]*85
 a=[]
-for i in range(93):
+for i in range(85):
     aa=[]
-    for j in range(93):
+    for j in range(85):
         b=f.read(2)
         aaa=b[0]*256+b[1]
         aa=aa+[aaa]
@@ -26,7 +26,7 @@ while True:
     if i==0:
         break
     #c=c+[i+(227*256+130)*256+160+(i>32)*192]
-    c=c+[227,130+(i>31),i+160-(i>31)*64]
+    c=c+[227,129+(i>63),i+128-(i>63)*64]
     front=i
 #print(c)
 cc=bytes(c).decode("utf-8")
